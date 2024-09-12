703720862cec73f3887ca2f9e587473b4809b68f
Coding Mantras <rampalsinghpunia@gmail.com>
2024-09-12 21:41:24 +0530

    Add order urls

---
+++ apps/orders/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.OrderListView.as_view(), name='order_list'),
+    path('<int:pk>/', views.OrderDetailView.as_view(), name='order_detail'),
+    path('create/', views.OrderCreateView.as_view(), name='order_create'),
+    path('<int:pk>/update/', views.OrderUpdateView.as_view(), name='order_update'),
+    path('<int:pk>/delete/', views.OrderDeleteView.as_view(), name='order_delete'),
+]
