bdb189b71d2cda6eaf094789cda563f5d1bc3833
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-27 11:31:09 +0000

    Delete: apps/support_agent/views.py
    ✨ Auto-committed on 2026-05-27 11:31:09

--- apps/support_agent/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-from django.urls import reverse_lazy
-
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-
-class SupportAgentTemplateView(LoginRequiredMixin, TemplateView):
-    template_name = 'support_agent/support_agent_chat.html'
