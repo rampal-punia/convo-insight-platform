7e6aa2ae125f4d8a5aef0670abdf5355a7b31a12
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-27 11:29:31 +0000

    Rename: backend/config/settings/stage.py
    ✨ Auto-committed on 2026-05-27 11:29:31

---
+++ backend/config/settings/stage.py
@@ -0,0 +1,29 @@
+# config/settings/stage.py
+
+from .base import *  # noqa
+from decouple import config
+
+ALLOWED_HOSTS = ["*"]
+INSTALLED_APPS += ["gunicorn", ]
+
+# Postgres
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', 'localhost'),
+        'PORT': config('DB_PORT'),
+    }
+}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Social
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
