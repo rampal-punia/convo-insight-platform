f3f81d05444b00225369da7e5b16d1d4f775ea84
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-27 12:42:13 +0000

    Update: frontend/tailwind.config.js
    ✨ Auto-committed on 2026-05-27 12:42:13

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ['./app/**/*.{js,jsx}', './lib/**/*.{js,jsx}'],
   theme: { extend: {} },
   plugins: [],
 };
