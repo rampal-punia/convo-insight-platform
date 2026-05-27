4622592632e8491567f93227171c2cd9d02b8094
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-27 11:31:06 +0000

    Delete: apps/analysis/admin.py
    ✨ Auto-committed on 2026-05-27 11:31:06

--- apps/analysis/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import LLMAgentPerformance, IntentPrediction
-
-
-@admin.register(IntentPrediction)
-class IntentPredictionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'intent')
