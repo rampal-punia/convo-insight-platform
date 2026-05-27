d189eaf8ca8fed3268fc6907445dcf7c6612a897
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-27 11:31:08 +0000

    Delete: apps/support_agent/routing.py
    ✨ Auto-committed on 2026-05-27 11:31:08

--- apps/support_agent/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(
-        r'ws/support_agent/(?P<conversation_id>[^/]+)?/$', consumers.SupportAgentConsumer.as_asgi()),
-]
