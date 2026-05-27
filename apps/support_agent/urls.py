ac028082dc4911063beb6a911e1fd30329e70c57
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-27 11:31:09 +0000

    Delete: apps/support_agent/urls.py
    ✨ Auto-committed on 2026-05-27 11:31:09

--- apps/support_agent/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'support_agent'
-
-urlpatterns = [
-    path('', views.SupportAgentTemplateView.as_view(),
-         name='support_agent_chat_url'),
-]
