9f0e8a30a1a130a36aabc6d300915e46c57b7770
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-27 04:26:37 +0000

    Update: apps/dashboard/urls.py
    ✨ Auto-committed on 2026-05-27 04:26:37

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -2,17 +2,10 @@
 
 from django.urls import path
 from . import views
+from .health import health_check, readiness_check
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path("", views.Dashboard.as_view(), name='home_url'),
-    # path("", views.ConversationListView.as_view(), name='conversation_list_url'),
-    # path('chat/', views.ConversationDetailView.as_view(),
-    #      name='new_conversation_url'),
-    # path('chat/<uuid:pk>/', views.ConversationDetailView.as_view(),
-    #      name='conversation_detail_url'),
-    # path("<uuid:pk>/delete/", views.ConversationDeleteView.as_view(),
-    #      name='conversation_delete_url'),
-    # path("new/", views.FinchatView.as_view(), name='new_chat_url'),
 ]
