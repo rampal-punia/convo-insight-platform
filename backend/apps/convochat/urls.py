a062523d8df547ba5b0c986bdcf61fe9c7d6ae6e
Coding Mantras <rampalsinghpunia@gmail.com>
2026-05-27 11:29:29 +0000

    Rename: backend/apps/convochat/urls.py
    ✨ Auto-committed on 2026-05-27 11:29:29

---
+++ backend/apps/convochat/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+app_name = 'convochat'
+
+urlpatterns = [
+    path("", views.ConversationListView.as_view(), name='conversation_list_url'),
+    path('chat/', views.ConversationDetailView.as_view(),
+         name='new_conversation_url'),
+    path('chat/<uuid:pk>/', views.ConversationDetailView.as_view(),
+         name='conversation_detail_url'),
+    path("<uuid:pk>/delete/", views.ConversationDeleteView.as_view(),
+         name='conversation_delete_url'),
+    path('chat/<uuid:pk>/sentiment/', views.ConversationSentimentView.as_view(),
+         name='conversation_sentiment_url'),
+    path('chat/<uuid:pk>/intent/', views.ConversationIntentView.as_view(),
+         name='conversation_intent_url'),
+    path('chat/<uuid:pk>/topics/', views.ConversationTopicView.as_view(),
+         name='conversation_topics_url'),
+    path("new/", views.ConvoChatView.as_view(), name='new_chat_url'),
+]
